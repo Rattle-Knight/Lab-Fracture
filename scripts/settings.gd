0f0776820a8ac68e0b350774570f7860a4cf664a
areiher02 <areiher02@gmail.com>
2024-08-12 13:37:49 -0700

    a rlly bad pause menu

---
+++ scripts/settings.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+
+
+func _on_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(0, value)
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(1, value)
+
+func _on_option_button_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600, 900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+
